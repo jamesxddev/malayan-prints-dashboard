91b156ca4d0cf519ee7c5c8be8587ac02ef47bc7
jamesxddev <jamescabral.dev@gmail.com>
2025-03-07 14:17:42 +0800

    added sidebar

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import AppSidebar from '@/components/AppSidebar.vue'
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import AppSidebar from "@/components/AppSidebar.vue";
 </script>
 
 <template>
